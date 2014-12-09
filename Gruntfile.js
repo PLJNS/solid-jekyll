ff2cb7ab7694c4a27af9161aab7e20ef4de83225
PLJNS <paulmmj@gmail.com>
2014-12-08 21:52:14 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "app/assets/css/style.css": "style.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+};
